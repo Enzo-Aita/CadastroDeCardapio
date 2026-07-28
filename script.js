12e27dfa344621dac071a41cf31f562e1c2b77f5
Enzo-Aita <enzoaitanaime99@gmail.com>
2026-07-28 12:43:32 -0300

    add card dashboard/documentação do script

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-
 // C >> Create  >> Cadastrar
 // R   >> Read   >> Ler
 // U   >>  Update  >> alterar/atualizar/editar/fuçar/mudar
 // D   >>  Delete  >> Apagar/deletar/excluir
 
-// ctrl + ;
-
-// const nomes = []
-// const alturas = []
-
-
-
-
 
 let cardapio = []
 
+// função de salvar dados no localStorege
 function salvarDados() {
     localStorage.setItem('cardapio', JSON.stringify(cardapio))
 
 }
-
+//função de carregar os dados salvos no localStorege
 function carregarDados() {
 
     cardapio = JSON.parse(localStorage.getItem('cardapio')) || []
@@ -27,7 +19,7 @@ function carregarDados() {
     // let textoLido = localStorage.getItem('cardapio')
     // cardapio = JSON.parse(textoLido)
 }
-
+//função de CREATE: Cadastrar
 function cadastrarProduto() {
 
     carregarDados()
@@ -50,44 +42,35 @@ function cadastrarProduto() {
 
 }
 
-function limparFormulario() {
-    document.getElementById('input-nome').value = ''
-    document.getElementById('input-preco').value = ''
-    document.getElementById('input-categoria').value = ''
-    document.getElementById('input-quantidade').value = ''
-    document.getElementById('input-tamanho').value = ''
-    document.getElementById('input-id').value = ''
-
-    document.getElementById('input-nome').focus()
-}
-
+//função READ: Mostrar todos os produtos cadastrados
 function mostrarTodos() {
     carregarDados()
-
-    document.getElementById('painel-cardapio').innerHTML = ''
-
+    
+    document.getElementById("grupo").style.display = "none"
+    document.getElementById('painel-cardapio').innerHTML = ""
+    
     for (let i = 0; i < cardapio.length; i++) {
         // alert(cardapio[i].nome)
         document.getElementById('painel-cardapio').innerHTML +=
-            `<div class="card-dino">
-            <h2>${cardapio[i].nome}</h2>
-            <p>Preco: R$ ${cardapio[i].preco}</p>
-            <p>Categoria: ${cardapio[i].categoria}</p>
-            <p>Quantidade: ${cardapio[i].quantidade}</p>
-            <p>Tamanho: ${cardapio[i].tamanho}</p>
-            <p>${cardapio[i].id}</p>
+        `<div class="card-dino">
+        <h2>${cardapio[i].nome}</h2>
+        <p>Preco: R$ ${cardapio[i].preco}</p>
+        <p>Categoria: ${cardapio[i].categoria}</p>
+        <p>Quantidade: ${cardapio[i].quantidade}</p>
+        <p>Tamanho: ${cardapio[i].tamanho}</p>
+        <p>${cardapio[i].id}</p>
         
         </div>
         `
     }
 
-
+    
 }
 
-
+//funcção pesquisar por produto
 function pesquisar() {
     let nomeProcurado = document.getElementById('input-nome').value
-
+    
     for (let i = 0; i < cardapio.length; i++) {
         if (nomeProcurado == cardapio[i].nome) {
             console.log(cardapio[i]);
@@ -98,25 +81,26 @@ function pesquisar() {
             document.getElementById('input-id').value = cardapio[i].id
             console.log(i);
         }
-
+        
     }
     salvarDados()
     carregarDados()
-
+    
 }
 
+// função UPDATE salvar/alterar produto
 function salvarProduto() {
     let id = Number(document.getElementById('input-id').value)
 
     for (let i = 0; i < cardapio.length; i++) {
         if (id == cardapio[i].id) {
-            console.log(cardapio[i]);
+            // console.log(cardapio[i]);
             cardapio[i].preco = document.getElementById('input-preco').value
             cardapio[i].categoria = document.getElementById('input-categoria').value
             cardapio[i].quantidade = document.getElementById('input-quantidade').value
             cardapio[i].tamanho = document.getElementById('input-tamanho').value
             cardapio[i].id = document.getElementById('input-id').value
-            console.log(i);
+            // console.log(i);
         }
     }
     salvarDados()
@@ -124,9 +108,10 @@ function salvarProduto() {
     limparFormulario()
 }
 
+//função DELETE: Deletar um item cadastrado
 function excluirProduto() {
     let id = Number(document.getElementById('input-id').value)
-
+    
     for (let i = 0; i < cardapio.length; i++) {
         if (id == cardapio[i].id) {
             console.log(cardapio[i]);
@@ -139,26 +124,27 @@ function excluirProduto() {
     limparFormulario()
 }
 
+//função do dashboard: abrir card
 function dashboard() {
 
     carregarDados()
-
+    
     document.getElementById("grupo").style.display = "none"
     document.getElementById("painel-botoes").style.display = "none"
     document.getElementById("painel-cardapio").style.display = "none"
-
+    
     if (cardapio.length == 0) {
         document.getElementById("painel-dashboard").style.display = "block"
         document.getElementById("painel-dashboard").innerHTML = ""
 
         "<h2>Não existe produtos cadastrados</h2>"
-
+        
         return
     }
-
+    
     document.getElementById("painel-dashboard").style.display = "block";
 
-
+    
     let produtoMaisCaro = cardapio[0]
     let produtoMaisBarato = cardapio[0]
     let quantidadeTotal = 0
@@ -167,7 +153,7 @@ function dashboard() {
     let lanches = 0
     let sobremesas = 0
     let outros = 0
-    
+
     
     
     
@@ -177,60 +163,67 @@ function dashboard() {
         }
     }
     document.getElementById("mais-caro").innerHTML = produtoMaisCaro.nome + " - R$ " + produtoMaisCaro.preco;
-
+    
     for (let i = 1; i < cardapio.length; i++) {
         if (cardapio[i].preco < produtoMaisBarato.preco) {
             produtoMaisBarato = cardapio[i]
         }
     }
     document.getElementById("mais-barato").innerHTML = produtoMaisBarato.nome + " - R$ " + produtoMaisBarato.preco;
-
+    
     for (let i = 0; i < cardapio.length; i++) {
-
+        
         quantidadeTotal += Number(cardapio[i].quantidade)
-
+        
         somaPrecos += Number(cardapio[i].preco)
-
+        
         if (cardapio[i].categoria == "Bebida") {
             bebidas++
         } else if (cardapio[i].categoria == "Lanches") {
             lanches++
         } else if (cardapio[i].categoria == "Sobremesas") {
             sobremesas++
-        } else {
-            outros++
         }
     }
-
+    
     let mediaPreco = somaPrecos / cardapio.length
-
+    
     document.getElementById("total-produtos").innerHTML = cardapio.length
-
+    
     document.getElementById("total-itens").innerHTML = quantidadeTotal
-
-    document.getElementById("media-preco").innerHTML = mediaPreco.toFixed(2)
-
+    
+    document.getElementById("media-preco").innerHTML = "R$" + mediaPreco.toFixed(2)
+    
     document.getElementById("bebidas").innerHTML = bebidas
-
+    
     document.getElementById("lanches").innerHTML = lanches
-
+    
     document.getElementById("sobremesas").innerHTML = sobremesas
-
-    document.getElementById("outros").innerHTML = outros
-
+    
 }
 
-
-
+//função fechar cardápio
 function fechar() {
     document.getElementById('painel-cardapio').innerHTML = ""
+    document.getElementById("grupo").style.display = ""
 }
 
+//função fechar daschboard
 function fecharDashboard() {
     document.getElementById("painel-dashboard").style.display = "none"
     document.getElementById("grupo").style.display = ""
     document.getElementById("painel-botoes").style.display = ""
     document.getElementById("painel-cardapio").style.display = ""
+    
+}
+function limparFormulario() {
+    document.getElementById('input-nome').value = ''
+    document.getElementById('input-preco').value = ''
+    document.getElementById('input-categoria').value = ''
+    document.getElementById('input-quantidade').value = ''
+    document.getElementById('input-tamanho').value = ''
+    document.getElementById('input-id').value = ''
 
+    document.getElementById('input-nome').focus()
 }
 
